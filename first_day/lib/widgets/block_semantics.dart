0e3f107ea5badf667d16a832b8cce10a1bfb07f2
sunny <sunny_ryu@outlook.com>
2022-06-18 15:29:24 +0900

    minor change

--- first_day/lib/widgets/block_semantics.dart
+++ first_day/lib/widgets/block_semantics.dart
@@ -9,16 +9,17 @@ class ContainerBlockSemantics extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       width: double.infinity,
       height: 200,
-      child: BlockSemantics(
+      child: const BlockSemantics(
         // BlockSemantics
         // 동일한 semantics 컨테이너에서 이전에 그려진 모든 위젯의 semantics를 삭제하는 위젯
         // 특정 위젯 뒤에 그려진 접근성 도구에서 위젯을 숨기는 데 유용
         // ex) Drawer은 drawer 외부 모든 위젯과 상호작용을 차단함
 
         blocking: true,
-        child: const CircleAvatar(
+        child: CircleAvatar(
           radius: 25.0,
-          backgroundImage: NetworkImage("https://post-phinf.pstatic.net/MjAyMTAxMjVfMTA1/MDAxNjExNTU3Mzk4MjIy.MEUMpAkus932Fhh-pbLvJrTfgcRSa9lLqnSzItTVjOcg.ua3bCg2MM04cjQR1LUjdol1vgYEhnSmUF-BMDWyZW7Ig.PNG/%EA%B7%80%EC%97%AC%EC%9A%B4%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png?type=w1200"),
+          backgroundImage: NetworkImage(
+              "https://post-phinf.pstatic.net/MjAyMTAxMjVfMTA1/MDAxNjExNTU3Mzk4MjIy.MEUMpAkus932Fhh-pbLvJrTfgcRSa9lLqnSzItTVjOcg.ua3bCg2MM04cjQR1LUjdol1vgYEhnSmUF-BMDWyZW7Ig.PNG/%EA%B7%80%EC%97%AC%EC%9A%B4%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8.png?type=w1200"),
           child: Text(
             "우리 고양이3",
             style: TextStyle(color: Colors.red),
